aba9f37560e1c922321dd1107d9785e98e4da076
yhobany <68508255+yhobany@users.noreply.github.com>
2026-06-08 11:05:30 -0500

    Corregir redirección inicial en SeleccionPerfilScreen para enviar a SetupFamiliaScreen si la lista de perfiles de hijos está vacía

--- lib/ui/screens/seleccion_perfil_screen.dart
+++ lib/ui/screens/seleccion_perfil_screen.dart
@@ -22,15 +22,15 @@ class SeleccionPerfilScreen extends StatelessWidget {
       return const Scaffold(body: Center(child: CircularProgressIndicator()));
     }
 
-    if (!authProv.existeAdmin || !authProv.estaAutenticado) {
+    final perfiles = perfilesProv.todosLosPerfiles;
+
+    if (!authProv.existeAdmin || !authProv.estaAutenticado || perfiles.isEmpty) {
       return const SetupFamiliaScreen();
     }
 
     if (!authProv.estaAprobado) {
       return const EsperandoAprobacionScreen();
     }
-
-    final perfiles = perfilesProv.todosLosPerfiles;
 
     return Scaffold(
       body: Container(
